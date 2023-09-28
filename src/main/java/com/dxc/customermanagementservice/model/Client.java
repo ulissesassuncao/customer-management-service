757c00f22f53982993876d22e86d027b5c1eb794
ulissesassuncao <ulisses.silva14@gmail.com>
2023-09-28 18:12:59 -0300

    Add 'Client' class to 'com.dxc.customermanagementservice.model'
        This class represents a client and is mapped to the 'client_tb' table in the database. It includes fields like 'id,' 'firstName,' 'lastName,' 'email,' 'cpf,' and 'birthDate' to store client
    information.
    
        Key functionalities include accessor methods to get and set these fields, along with a constructor allowing instances of the class to be created with all client details.

---
+++ src/main/java/com/dxc/customermanagementservice/model/Client.java
@@ -0,0 +1,88 @@
+package com.dxc.customermanagementservice.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client_tb")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    public Client() {
+    }
+
+    public Client(String firstName, String lastName, String email, String cpf, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.cpf = cpf;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+}
